fd1a55e96cde43a3c3e0063a8fd6191be344b337
Austin Nhung <austinnhung@yahoo.ca>
2022-05-02 02:10:03 -0600

    Changes to documentation and variable names

--- script2py/nodes.py
+++ script2py/nodes.py
@@ -2,23 +2,30 @@
 
 The nodes each represent a block in the script. The currently supported nodes
 are:
+
 - the base `Node`, which all nodes inherit from
 - the `Line` node, which stores a a line of dialogue from a speaker
 - the `Choice` node, which gives the player a choice and creates a branching
   path
 - the `Setter` node, which changes a system parameter
+
 """
+
+
+import textwrap
 
 
 class Node:
     """The base node class.
 
     Each node has the following properties:
+
     - a `node_id`, which is created upon instantiation
     - a `section`, which groups nodes together
     - a `next_section`, which is only applicable for the last node in a section
 
     Each inherited class should:
+
     - set the `node_type`
     - format its displayed text using the `_format()` function
     - add to the `to_dot()` and `to_json()` functions with its additional
@@ -26,25 +33,41 @@ class Node:
 
     Parameters
     ------------
-    section:        (str) the name of the group this node is in.
-    textlength_max: (int) the maximum number of characters per line of text.
+    next_id: str
+        the node id of the next node. The last node should be left as None.
+    section: str
+        the name of the group this node is in.
+    next_section: str
+        the name of the next section. If the next node is in the same section,
+        this should be None.
+    wrap: int
+        the maximum number of characters per line of text. Default wrapping
+        width is 80.
     """
-    node_type = None
+    node_type = "node"
     
-    def __init__(self, section, textlength_max=80):
-        self.node_id = id(self)
+    def __init__(
+        self, 
+        next_id: str = None, 
+        section: str = "", 
+        next_section: str = None, 
+        wrap: int = 80
+    ):
+        self.node_id = str(id(self))
+        self.next_id = next_id
 
         self.section = section
-        self.textlength_max = textlength_max
+        self.next_section = next_section
 
-        self.next_section = None
+        self.wrap = wrap
 
     def to_dot(self):
         """Formats the node parameters for a dot graph.
 
         Returns
         ---------
-        (str) the dot graph for this node.
+        str
+            the dot graph for this node.
         """
         return f"{self.node_id};"
 
@@ -53,66 +76,81 @@ class Node:
 
         Returns
         ---------
-        (str) the JSON data for this node.
+        str
+            the JSON data for this node.
         """
         return {
-            "id": self.node_id,
-            "type": self.node_type
+            "node_id": self.node_id,
+            "type": self.node_type,
+            "next_id": self.next_id,
         }
 
-    def _format(self, text):
-        """Formats text to have a a maximum of `textlength_max` characters.
+    def _clean(self, text: str):
+        """Cleans a block of text by removing the newlines and applying the
+        correct wrapping.
 
         Parameters
         ------------
-        text: (str) the text to be formatted.
+        text: str
+            the block of text.
 
         Returns
         ---------
-        (str) the formatted text.
+        str
+            the cleaned line of text.
         """
-        textlength = 0
-        while len(text) - textlength > self.textlength_max:
-            textlength += self.textlength_max
-            
-            for i in range(self.textlength_max):
-                if text[textlength-i].isspace():
-                    text = text[0:textlength-i] + "\n" + text[textlength-i+1:]
-                    textlength += 1
-                    break
-            
-        return text
+        cleaned =  " ".join([line.strip() for line in text.strip().split("\n")])
+        wrapped = "\n".join(textwrap.wrap(text=cleaned, width=self.wrap))
+
+        return wrapped
 
 
 class Line(Node):
     """Displays a line from the specified speaker.
 
     Script format:
+
     ```
-    Name: Line of text.
+    SpeakerName: Line of text.
+    SpeakerName: Another line of text. This one extends to
+        more than one line.
     ```
 
     JSON format:
+
     ```
     {
-        "id": "0001",
+        "node_id": "0001",
         "type": "line",
-        "speaker": "Name",
+        "speaker": "SpeakerName",
         "text": "Line of text.",
-        "next": "0002"
+        "next_id": "0002"
+    },
+    {
+        "node_id": "0002",
+        "type": "line",
+        "speaker": "SpeakerName",
+        "text": "Another line of text. This one extends to more than one line.",
+        "next_id": null
     }
     ```
 
-    speaker: (str) name of the character speaking.
-    text: (str) the line of text the character is saying.
+    Parameters
+    ------------
+    speaker: str
+        name of the character speaking.
+    text: str
+        the line of text the character is saying.
+    kwargs:
+        keyword arguments passed to the base class `Node`.
     """
     node_type = "line"
 
-    def __init__(self, speaker="", text="", **kwargs):
+    def __init__(self, speaker: str = "", text: str = "", **kwargs):
         super().__init__(**kwargs)
 
         self.speaker = speaker
-        self.text = self._format(text)
+        self.text = self._clean(text)
 
     def to_dot(self):
         return f"{self.node_id} [label=\"{self.speaker}\\n{self.text}\", shape=box];"
@@ -131,44 +169,76 @@ class Choice(Node):
     """Offers a choice to the player.
 
     Script format:
+
     ```
-    CHOICE
-        First Choice: 0001
-        Second Choice: 0002
+    *** SpeakerName: First Choice
+        SpeakerName: Another line
+        --> Branch1
+    *** SpeakerName: Second Choice
     ```
 
     JSON format:
+
     ```
     {
-        "id": "0001",
+        "node_id": "0001",
         "type": "choice",
-        "choices": {
-            "First Choice": "0001",
-            "Second Choice": "0002"
-        }
+        "choices": [
+            {
+                "speaker": "SpeakerName",
+                "choice": "First Choice",
+                "next_id": "0002"
+            },
+            {
+                "speaker": "SpeakerName",
+                "choice": "Second Choice",
+                "next_id": "0003"
+            }
+        ],
+        "next_id": "0003"
+    },
+    {
+        "node_id": "0002",
+        "type": "line",
+        "speaker": "SpeakerName",
+        "text": "Another line",
+        "next_id": "0004"
     }
     ```
 
-    choices: (list) each element is a single key-value dictionary mapping the
-        choice text to the id of the next node.
+    If no branch is specified, the next node will be assumed to be the next
+    node in the script.
+
+    Parameters
+    ------------
+    choices: list(dict)
+        each element is a dictionary with keys "speaker", "text", and
+        (optionally) "next_id".
+    kwargs:
+        keyword arguments passed to the base class `Node`.
     """
     node_type = "choice"
 
-    def __init__(self, choices=dict(), **kwargs):
+    def __init__(self, choices: list = list(), **kwargs):
         super().__init__(**kwargs)
 
-        self.choices = {key: self._format(value) for key, value in choices.items()}
+        self.choices = choices
+
+        for choice in self.choices:
+            choice["speaker"] = choice.get("speaker", "")
+            choice["text"] = self._clean(choice.get("text", ""))
+            choice["next_id"] = choice.get("next_id")
 
     def to_dot(self):
         choice_text = "\\n".join([
-            f"{n}. {choice}" for n, choice in enumerate(self.choices.keys())
+            f"{n}. {choice}" for n, choice in enumerate(self.choices)
         ])
 
         return f"{self.node_id} [label=\"Choice\\n{choice_text}\", shape=box];"
 
     def to_json(self):
         json_node = super().to_json()
-        json_node.update({"choices": [choice for choice in self.choices.keys()]})
+        json_node.update({"choices": [choice for choice in self.choices]})
 
         return json_node
 
@@ -177,26 +247,44 @@ class Setter(Node):
     """Sets a variable to a value.
 
     Script format:
+
     ```
-    VariableName: variable value
+    <<{ variable_name = "variable value" }>>
+    <<{ variable_name = 10 }>>
     ```
 
     JSON format
     ```
     {
-        "id": "0001",
+        "node_id": "0001",
         "type": "setter",
-        "properties": {
-            "VariableName": "variable value"
-        }
+        "setter": {
+            "variable_name": "variable value"
+        },
+        "next_id": "0002"
+    },
+    {
+        "node_id": "0002",
+        "type": "setter",
+        "setter": {
+            "variable_name": 10
+        },
+        "next_id": null
     }
     ```
 
-    properties: (dict) key-value pairs of variables.
+    Parameters
+    ------------
+    key: str
+        the variable name.
+    value: str or int or bool
+        the value of the variable name.
+    kwargs:
+        keyword arguments passed to the base class `Node`.
     """
     node_type = "setter"
 
-    def __init__(self, key="", value="", **kwargs):
+    def __init__(self, key: str = "", value: str | int | bool = "", **kwargs):
         super().__init__(**kwargs)
 
         self.key = key
@@ -213,4 +301,3 @@ class Setter(Node):
         })
 
         return json_node
-
